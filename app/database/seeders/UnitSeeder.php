646dffc815a12e645d760423440426d105e3ec04
mathiconical <netinho340@gmail.com>
2025-04-06 11:03:38 -0300

    add unit seeder

--- app/database/seeders/UnitSeeder.php
+++ app/database/seeders/UnitSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UnitSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class UnitSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('units')->insert([
+            [
+                'abbr' => 'KG',
+                'name' => 'Kilograma',
+            ],
+            [
+                'abbr' => 'UN',
+                'name' => 'Unidade',
+            ],
+        ]);
     }
 }
