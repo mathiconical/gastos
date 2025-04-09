6599399b1c863a9d2e9e49616556c5836bf10ce9
mathiconical <netinho340@gmail.com>
2025-04-09 18:13:25 -0300

    update: temporary redirect

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // temporary redirect until the example page its finish
+    return redirect()->to('/admin');
 });
