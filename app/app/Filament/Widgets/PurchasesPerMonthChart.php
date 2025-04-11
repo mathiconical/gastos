5b093a35e630e9cf01ced21a367d6fac5092b90f
mathiconical <netinho340@gmail.com>
2025-04-10 21:36:03 -0300

    update: add a bar widget on dashboard

--- app/app/Filament/Widgets/PurchasesPerMonthChart.php
+++ app/app/Filament/Widgets/PurchasesPerMonthChart.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Filament\Resources\Widgets;
+namespace App\Filament\Widgets;
 
+use App\Models\Purchase;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
 
 class PurchasesPerMonthChart extends ChartWidget
 {
@@ -10,11 +12,45 @@ class PurchasesPerMonthChart extends ChartWidget
 
     protected function getData(): array
     {
-        return [];
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Compras Mensais',
+                    'data' => $this->fillValuesIntoMonthsArray(),
+                ],
+            ],
+            'labels' => $this->getMonthsArray(),
+        ];
     }
 
     protected function getType(): string
     {
         return 'bar';
+    }
+
+    private function fillValuesIntoMonthsArray(): array
+    {
+        $purchases = Purchase::query()
+            ->selectRaw("SUM(total) AS total, strftime('%m', date) AS monthDate")
+            ->groupBy(DB::raw("strftime('%m', date)"))
+            ->get()
+            ->toArray();
+
+        $monthsArray = $this->getMonthsArray();
+
+        $data = [];
+
+        foreach ($purchases as $purchase) {
+            $monthIntIndex = intval($purchase['monthDate']) - 1;
+            $monthAbbrKey = $monthsArray[$monthIntIndex];
+            $data[$monthAbbrKey] = $purchase['total'];
+        }
+
+        return $data;
+    }
+
+    private function getMonthsArray(): array
+    {
+        return explode(',', 'Jan,Fev,Mar,Abr,Mai,Jun,Jul,Ago,Set,Out,Nov,Dez');
     }
 }
