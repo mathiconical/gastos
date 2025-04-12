e16c100d8f4bae6cff9cafea2900ef7a0f5d2da7
mathiconical <netinho340@gmail.com>
2025-04-12 10:49:43 -0300

    add widget dashboard

--- app/app/Filament/Widgets/PurchasesPerMonthChart.php
+++ app/app/Filament/Widgets/PurchasesPerMonthChart.php
@@ -4,11 +4,30 @@ namespace App\Filament\Widgets;
 
 use App\Models\Purchase;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class PurchasesPerMonthChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Ultimos 12 Meses';
+
+    protected static string $color = 'danger';
+
+    public ?string $filter = '2024';
+
+    protected static ?string $pollingInterval = '10s';
+
+    protected function getFilters(): ?array
+    {
+        return Purchase::query()
+            ->get()
+            ->map(fn($item): array => ['year' => $item->date->year])
+            ->unique()
+            ->values()
+            ->pluck('year', 'year')
+            ->prepend('Ultimos 12 Meses', 'lastYearInterval')
+            ->toArray();
+    }
 
     protected function getData(): array
     {
@@ -17,6 +36,7 @@ class PurchasesPerMonthChart extends ChartWidget
                 [
                     'label' => 'Compras Mensais',
                     'data' => $this->fillValuesIntoMonthsArray(),
+                    'borderColor' => '#dc2662',
                 ],
             ],
             'labels' => $this->getMonthsArray(),
@@ -28,20 +48,39 @@ class PurchasesPerMonthChart extends ChartWidget
         return 'bar';
     }
 
+    private function applyFilter(Collection $purchases): Collection
+    {
+        if ($this->filter === 'lastYearInterval') {
+            return $purchases->filter(function ($item) {
+                return $item['date'] >= now()->subYear();
+            });
+        }
+
+        return $purchases->filter(function ($item) {
+            return substr($item['date'], 0, 4) == $this->filter;
+        });
+    }
+
     private function fillValuesIntoMonthsArray(): array
     {
         $purchases = Purchase::query()
-            ->selectRaw("SUM(total) AS total, strftime('%m', date) AS monthDate")
-            ->groupBy(DB::raw("strftime('%m', date)"))
+            ->selectRaw("total, date")
             ->get()
-            ->toArray();
+            ->map(function ($item) {
+                return [
+                    'total' => $item->total,
+                    'date' => $item->date->format('Y-m-d')
+                ];
+            });
+
+        $purchases = $this->applyFilter($purchases);
 
         $monthsArray = $this->getMonthsArray();
 
         $data = [];
 
         foreach ($purchases as $purchase) {
-            $monthIntIndex = intval($purchase['monthDate']) - 1;
+            $monthIntIndex = intval(substr($purchase['date'], 5, 2)) - 1;
             $monthAbbrKey = $monthsArray[$monthIntIndex];
             $data[$monthAbbrKey] = $purchase['total'];
         }
